a7584c815062320347559fe0327c990a7ec6c0b9
xiaobing1103 <472427503@qq.com>
2023-03-23 14:39:36 +0800

    修改yaml文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,14 @@
 const path = require('path')
 module.exports = {
   base: '/man-super-ui/',
-  publicPath: '/man-super-ui/',
   title: 'Super UI',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@assets': '/man-super-ui/assets',
+      },
+    },
+  },
   description: 'Inspiration from element-ui',
   head: [['link', { rel: 'icon', href: '/app.ico' }]],
   themeConfig: {
