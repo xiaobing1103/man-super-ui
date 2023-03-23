a0b94bc481fa915e75dbdd5a5354338229dcc8c2
xiaobing1103 <472427503@qq.com>
2023-03-23 14:01:51 +0800

    修改yaml文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,49 @@
 const path = require('path')
 module.exports = {
-    base:'/man-super-ui/',
-    title: 'Super UI',
-    description: 'Inspiration from element-ui',
-    head: [
-      ['link', { rel: 'icon', href: '/app.ico' }]
-    ],
-    themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: '支持', link: 'support.md' },
-      ],
-      sidebar: [
-          {
-              title: '开发指南',
-              collapsable: true,
-              children: [
-                'views/guide/install.md',
-                'views/guide/get-started.md'
-              ]
-            },
-            // {
-            //   title: '设计',
-            //   collapsable: true,
-            //   children: [
-            //     'views/design/color/',
-            //   ]
-            // },
-            {
-              title: '组件',
-              collapsable: true,
-              children: [
-                'views/components/basic/',
-                'views/components/form/',
-                'views/components/navigation/',
-                'views/components/notice/',
-                'views/components/other/'
-              ]
-            },
-        ]
+  base: '/man-super-ui/',
+  title: 'Super UI',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@assets': '/man-super-ui/assets',
       },
-    scss:{ //配置 scss 根目录
-      includePaths: [path.join(__dirname, '../../style')]
-    }
-  }
+    },
+  },
+  description: 'Inspiration from element-ui',
+  head: [['link', { rel: 'icon', href: '/app.ico' }]],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '支持', link: 'support.md' },
+    ],
+    sidebar: [
+      {
+        title: '开发指南',
+        collapsable: true,
+        children: ['views/guide/install.md', 'views/guide/get-started.md'],
+      },
+      // {
+      //   title: '设计',
+      //   collapsable: true,
+      //   children: [
+      //     'views/design/color/',
+      //   ]
+      // },
+      {
+        title: '组件',
+        collapsable: true,
+        children: [
+          'views/components/basic/',
+          'views/components/form/',
+          'views/components/navigation/',
+          'views/components/notice/',
+          'views/components/other/',
+        ],
+      },
+    ],
+  },
+  scss: {
+    //配置 scss 根目录
+    includePaths: [path.join(__dirname, '../../style')],
+  },
+}
